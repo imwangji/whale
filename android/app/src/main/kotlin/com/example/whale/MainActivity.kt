090c6535ced50392c37bc301060afbe2f77ccdb3
imwangji <86795863@qq.com>
2021-02-15 20:05:43 +0800

    A 初始化App框架

---
+++ android/app/src/main/kotlin/com/example/whale/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.whale
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.timer)),
+          BottomNavigationBarItem(icon: Icon(Icons.format_quote_outlined)),
+          BottomNavigationBarItem(icon: Icon(Icons.photo_outlined)),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle_outlined)),
+        ],
+      ),
+      tabBuilder: (context,index){
+        return Center(
+          child: Text(index.toString()),
+        );
+      },
+    );
+  }
+}

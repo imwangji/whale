4c381dcdc5cf6131ee2c07c1fb5460f52a9ae24e
imwangji <imwangji@qq.com>
2021-02-18 01:10:14 +0800

    U 一些计时器相关的代码

--- lib/page/timer_counting_page.dart
+++ lib/page/timer_counting_page.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:whale/component/timer_card.dart';
+import 'package:whale/provider/current_timer_card.dart';
 import 'package:whale/type/timer_card_configuration.dart';
 
 class TimerCountingPage extends StatefulWidget {
@@ -15,8 +19,24 @@ class TimerCountingPage extends StatefulWidget {
 class _TimerCountingPageState extends State<TimerCountingPage>
     with SingleTickerProviderStateMixin {
   AnimationController _controller;
-  bool isCounting = false;
-  DateTime startAt;
+  Timer timer;
+  String countingText;
+
+  String _printDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return "${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds";
+  }
+  String getDisplayCountingText() {
+    if(Provider.of<CurrentTimerCard>(context,listen: false).isCounting){
+      CurrentTimerCard currentTimerCard = Provider.of<CurrentTimerCard>(context,listen: false);
+      Duration durationTime = DateTime.now().difference(currentTimerCard.startAt);
+      return _printDuration(durationTime);
+    }else{
+      return "00:00:00";
+    }
+  }
   @override
   void initState() {
     super.initState();
@@ -29,13 +49,12 @@ class _TimerCountingPageState extends State<TimerCountingPage>
     _controller.dispose();
   }
 
-  Widget getCountingIcon(){
-    if(this.isCounting){
+  Widget getCountingIcon() {
+    if (Provider.of<CurrentTimerCard>(context,listen: false).isCounting) {
       return GestureDetector(
-        onTap: (){
-          setState(() {
-            this.isCounting = false;
-          });
+        onTap: () {
+          Provider.of<CurrentTimerCard>(context,listen: false).clearCounting();
+          this.timer.cancel();
         },
         child: Icon(
           CupertinoIcons.stop_circle,
@@ -43,11 +62,15 @@ class _TimerCountingPageState extends State<TimerCountingPage>
           color: CupertinoColors.systemPink,
         ),
       );
-    }else{
+    } else {
       return GestureDetector(
-        onTap: (){
-          setState(() {
-            this.isCounting = true;
+        onTap: () {
+          Provider.of<CurrentTimerCard>(context,listen: false).setTimerCardName(this.widget.timerCardConfiguration.name);
+          Provider.of<CurrentTimerCard>(context,listen: false).setStartTime(DateTime.now());
+          this.timer = Timer.periodic(Duration(seconds: 1), (timer) {
+            setState(() {
+              this.countingText = DateTime.now().difference(Provider.of<CurrentTimerCard>(context,listen: false).startAt).toString();
+            });
           });
         },
         child: Icon(
@@ -61,50 +84,59 @@ class _TimerCountingPageState extends State<TimerCountingPage>
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      child: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: Hero(
-                  tag: "timer_card",
-                  child: TimerCard(this.widget.timerCardConfiguration),
+
+    return Consumer<CurrentTimerCard>(
+      builder: (context, currentTimerCard, child) {
+        return CupertinoPageScaffold(
+          child: SafeArea(
+            child: Column(
+              children: [
+                Container(
+                  child: GestureDetector(
+                    onTap: () {
+                      this.timer.cancel();
+                      Navigator.pop(context);
+                    },
+                    child: Hero(
+                      tag: "timer_card",
+                      child: TimerCard(this.widget.timerCardConfiguration),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Expanded(
-              child: Flex(
-                direction: Axis.vertical,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "0:00:00",
-                    style: TextStyle(
-                      fontSize: 46,
-                      fontWeight: FontWeight.bold,
-                    ),
+                Expanded(
+                  child: Flex(
+                    direction: Axis.vertical,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        this.getDisplayCountingText(),
+                        style: TextStyle(
+                          fontSize: 46,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Container(
+                        height: 20,
+                      ),
+                      getCountingIcon(),
+                      Container(
+                        height: 50,
+                      ),
+                      Text(
+                        "回到主界面不会打断计时",
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: CupertinoColors.secondaryLabel),
+                      ),
+                    ],
                   ),
-                  Container(height: 20,),
-                  getCountingIcon(),
-                  Container(height: 50,),
-                  Text(
-                    "回到主界面不会打断计时",
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: CupertinoColors.secondaryLabel
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/provider/current_timer_card.dart
+++ lib/provider/current_timer_card.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/cupertino.dart';
 
-class CurrentTimerCard extends ChangeNotifier{
+class CurrentTimerCard extends ChangeNotifier {
   String timerCardId;
+  String timerCardName;
   DateTime startAt;
   bool isCounting;
-  void setStartTime(DateTime startAt){
+
+  CurrentTimerCard({this.isCounting = false,this.timerCardName="测试"});
+
+  void setStartTime(DateTime startAt) {
     this.isCounting = true;
     this.startAt = startAt;
     notifyListeners();
   }
-  void clearCounting(){
+
+  void clearCounting() {
     this.isCounting = false;
+    this.setTimerCardName(null);
+    notifyListeners();
+  }
+
+  void setTimerCardName(String name) {
+    this.timerCardName = name;
     notifyListeners();
   }
 }
